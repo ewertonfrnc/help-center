c6d756f25359ab8c73ac90f846d7ca702700bf21
Ewerton Franco <ewerton.silva@usemobile.xyz>
2024-07-14 13:45:27 -0300

    fixed db connection string

--- database/db.config.js
+++ database/db.config.js
@@ -3,12 +3,12 @@ const { Sequelize } = require("sequelize");
 const devConnectionString =
   "postgres://postgres:1364@localhost:5432/support-desk";
 const prodConnectionString =
-  "postgresql://supportdeskdb_user:w5UpqIP3t213lGTr3LqbIzohF3oM95GK@dpg-cq8rkklds78s739515t0-a.oregon-postgres.render.com/supportdeskdb";
+  "postgresql://supportdeskdb_user:w5UpqIP3t213lGTr3LqbIzohF3oM95GK@dpg-cq8rkklds78s739515t0-a.oregon-postgres.render.com/supportdeskdb?ssl=true";
 
 const sequelize = new Sequelize(
   process.env.NODE_ENV === "production"
     ? prodConnectionString
-    : devConnectionString,
+    : devConnectionString
 );
 
 module.exports = sequelize;
