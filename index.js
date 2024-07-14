52b7d788c6a9d15a179fed24a1a9e9e39fb5ee24
Ewerton Franco <ewerton.silva@usemobile.xyz>
2024-07-14 13:41:54 -0300

    add log info

--- index.js
+++ index.js
@@ -58,5 +58,8 @@ app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(
+    `Server running on port ${PORT}`,
+    `NODE_ENV: ${process.env.NODE_ENV}`
+  );
 });
